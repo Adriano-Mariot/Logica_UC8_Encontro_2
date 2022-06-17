273e76f6ce1b99e66a5f5ef9fc174e87d0a9ef46
Adrino <adrianomariot@hotmail.com>
2022-06-17 16:09:56 -0300

    Alterado arquivo par_impar.js

--- par_impar.js
+++ par_impar.js
@@ -3,23 +3,22 @@
 // se o número for par, escreva 'par' e o número correspondente.
 // se o número for ímpar, escreva 'ímpar' e o número correspondente.
 // se o número for zero, escreva 'zero' e o número correspondente.
-console.log("\n---------------------------------------------------------------- ")
-console.log(" Alunos Par ou Ímpar - Por Adriano Mariot")
-console.log("---------------------------------------------------------------- \n")
+console.log("\n---------------------------------------------------------------- ");
+console.log(" Alunos Par ou Ímpar - Por Adriano Mariot");
+console.log("---------------------------------------------------------------- \n");
 const input = require("prompt-sync")();
-let numAlunos= input("Entre com o numero de alunos presentes no Encontro remoto : ")
-console.log("---------------------------------------------------------------- \n")
+let numAlunos= input("Entre com o numero de alunos presentes no Encontro remoto : ");
+console.log("---------------------------------------------------------------- \n");
 for (let contAlunos = 0; contAlunos <=numAlunos; contAlunos++){
     if(contAlunos==0){
-        console.log(contAlunos+"- O numero do Aluno "+contAlunos+" é Zero.");
-        console.log("----------------------------------- ")
-    }
-    if((contAlunos%2)==0){
+        console.log(contAlunos+" - O numero do Aluno "+contAlunos+" é Zero.");
+        console.log("----------------------------------- ");
+    }else if((contAlunos%2)==0){
         console.log(contAlunos+"° - O numero do Aluno "+contAlunos+" é par.");
-        console.log("----------------------------------- ")
+        console.log("----------------------------------- ");
     }else{
         console.log(contAlunos+"° - O numero do Aluno "+contAlunos+" é ímpar.");
-        console.log("----------------------------------- ")
+        console.log("----------------------------------- ");
     }
 }
-console.log("\n--------------- Fim -------------- ")
+console.log("\n--------------- Fim -------------- \n");
